f76f7b0ad9e62113abbba264c355fe85137057a5
Jithin <jithingopalakrishnan2004@gmail.com>
2026-02-04 15:15:02 +0530

    added sampleview url

--- cart/urls.py
+++ cart/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("cart/", views.CartView.as_view(), name="cart"),
     path("cart/item/<int:pk>/", views.CartItemDetailView.as_view(), name="cart-item"),
     path("count/",views.CartCountView.as_view(),name="cart-count"),
+    path('sample/',views.sampleView.as_view())
 
 
 
